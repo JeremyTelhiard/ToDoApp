61228e8dba919baf53aaf0b8f8e6bcada550aa01
Jeremy Telhiard <jeremy.telhiard@gmail.com>
2020-09-12 22:55:21 -0500

    Persist added lists

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 "use strict";
 
 const listContainer = document.querySelector('[data-lists]');
+const listForm = document.querySelector('[data-new-list]')
+const listInput = document.querySelector('[data-list-input]')
 
-let lists = [];
+
+const LOCAL_STORAGE_LIST_KEY = 'tasks.lists';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+
+listForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const name = listInput.value;
+  if(name == null || name === ''){
+    return;
+  }
+  const list = createList(name);
+  listInput.value = null;
+  lists.push(list);
+  saveAll();
+})
+
+function createList(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    tasks: []
+  }
+}
+
+function saveAll() {
+  save();
+  render();
+}
+
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+}
+
 
 function render() {
   clear(listContainer);
   lists.forEach(list => {
     const listElement = document.createElement('li');
-    listElement.innerText = list;
+    listElement.dataset.listId = list.id;
+    listElement.innerText = list.name;
     listContainer.appendChild(listElement);
   })
 }
